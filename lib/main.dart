71fb11167faf54a33113dba053fef46aabac256b
leesanguk <sangwookpen96@gmail.com>
2023-05-03 15:48:54 +0900

    플러터 앱 개발 시작

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'N.G 게임소개사이트',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'N.G')
+        title: 'N.G 게임소개사이트',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'N.G')
     );
   }
 }
@@ -29,13 +29,23 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{ //Single.. 이게 있어야 애니메이션 기능들을 할 수 있음
+  late TabController _tabController;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      () => setState(() => _selectedIndex = _tabController.index);
     });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
@@ -44,25 +54,28 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Center(child: Text(widget.title)), // N.G 중앙으로 오게함
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Container(),
+      bottomNavigationBar: TabBar(
+        controller: _tabController,
+        tabs: [
+          Tab(
+            icon : Icon(Icons.person),
+            text : "Friends"
+          ),
+          Tab(
+              icon : Icon(Icons.person),
+              text : "Friends"
+          ),
+          Tab(
+              icon : Icon(Icons.person),
+              text : "Friends"
+          ),
+          Tab(
+              icon : Icon(Icons.person),
+              text : "Friends"
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
+    }
 }

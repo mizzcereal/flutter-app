df6f76390c251b5d9fd2d9eda27e4eff5dd87c11
leesanguk <sangwookpen96@gmail.com>
2023-05-25 15:51:13 +0900

    import 도전기

---
+++ lib/job.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+
+class JobTab extends StatefulWidget {
+  @override
+  _JobTabState createState() => _JobTabState();
+}
+
+class _JobTabState extends State<JobTab> {
+  bool showBlueBox1 = false;
+  bool showBlueBox2 = false;
+  bool showBlueBox3 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          showBlueBox1 = !showBlueBox1;
+        });
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showBlueBox1 = !showBlueBox1;
+                    });
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage('images/ad_war.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '모험가 전사',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showBlueBox2 = !showBlueBox2;
+                    });
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage('images/ad_war.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '모험가 전사',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showBlueBox3 = !showBlueBox3;
+                    });
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ),
+                SizedBox(height: 30),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (showBlueBox1) _buildBlueBox1(),
+                  if (showBlueBox2) _buildBlueBox2(),
+                  if (showBlueBox3) _buildBlueBox3(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBlueBox1() {
+    return Container(
+      width: 400,
+      height: 600,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          '첫 번째 파란색 박스',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBlueBox2() {
+    return Container(
+      width: 400,
+      height: 600,
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          '두 번째 파란색 박스',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBlueBox3() {
+    return Container(
+      width: 400,
+      height: 600,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          '세 번째 파란색 박스',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'job.dart'; // job_tab.dart 파일을 임포트합니다.
 
 void main() {
   runApp(const MyApp());
@@ -129,184 +130,11 @@ class _MyAppBodyState extends State<_MyAppBody> {
               ),
             ],
           ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                showBlueBox1 = !showBlueBox1;
-              });
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: Column(
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            showBlueBox1 = !showBlueBox1;
-                          });
-                        },
-                        child: Container(
-                          width: 200,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 2,
-                            ),
-                            image: DecorationImage(
-                              image: AssetImage('images/ad_war.png'),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              '모험가 전사',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            showBlueBox2 = !showBlueBox2;
-                          });
-                        },
-                        child: Container(
-                          width: 200,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 2,
-                            ),
-                            image: DecorationImage(
-                              image: AssetImage('images/ad_war.png'),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              '모험가 전사',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            showBlueBox3 = !showBlueBox3;
-                          });
-                        },
-                        child: Container(
-                          width: 200,
-                          height: 100,
-                          color: Colors.red,
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (showBlueBox1) _buildBlueBox1(),
-                        if (showBlueBox2) _buildBlueBox2(),
-                        if (showBlueBox3) _buildBlueBox3(),
-                      ],
-                    ),
-                  ),
-
-                ),
-              ],
-            ),
-          ),
+          JobTab(), // job_tab.dart에서 임포트한 위젯을 추가합니다.
           Center(
             child: Text('네 번째 탭 화면'),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildBlueBox1() {
-    return Container(
-      width: 400,
-      height: 600,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '첫 번째 파란색 박스',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildBlueBox2() {
-    return Container(
-      width: 400,
-      height: 600,
-      decoration: BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '두 번째 파란색 박스',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildBlueBox3() {
-    return Container(
-      width: 400,
-      height: 600,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '세 번째 파란색 박스',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
       ),
     );
   }

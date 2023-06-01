e43d981a1bd6556f0ad5208dad07db1e19f4e9bd
leesanguk <sangwookpen96@gmail.com>
2023-06-01 18:23:23 +0900

    갈아엎었지만 롤백

--- lib/job.dart
+++ lib/job.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'adjob.dart';
 
 class JobTab extends StatefulWidget {
   @override
@@ -7,9 +9,10 @@ class JobTab extends StatefulWidget {
 
 class _JobTabState extends State<JobTab> {
   bool showAdWarrior = false;
-  bool showAdWizard = false;
+  bool showAdwizard = false;
   bool showAdBowman = false;
   bool showAdThief = false;
+  bool showAdPirate = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,137 +27,171 @@ class _JobTabState extends State<JobTab> {
         children: [
           Expanded(
             flex: 3,
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      showAdWarrior = !showAdWarrior;
-                    });
-                  },
-                  child: Container(
-                    width: 200,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage('images/ad_war.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '모험가 전사',
-                        style: TextStyle(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showAdWarrior = !showAdWarrior;
+                      });
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
                           color: Colors.black,
+                          width: 2,
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage('images/ad_war.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '모험가 전사',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 30),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      showAdWizard = !showAdWizard;
-                    });
-                  },
-                  child: Container(
-                    width: 200,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage('images/ad_bishop.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '모험가 마법사',
-                        style: TextStyle(
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showAdwizard = !showAdwizard;
+                      });
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
                           color: Colors.black,
+                          width: 2,
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage('images/ad_bishop.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '모험가 마법사',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 30),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      showAdBowman = !showAdBowman;
-                    });
-                  },
-                  child: Container(
-                    width: 200,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage('images/ad_bowmaster.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '모험가 궁수',
-                        style: TextStyle(
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showAdBowman = !showAdBowman;
+                      });
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
                           color: Colors.black,
+                          width: 2,
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage('images/ad_bowmaster.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '모험가 궁수',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 30),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      showAdThief = !showAdThief;
-                    });
-                  },
-                  child: Container(
-                    width: 200,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage('images/ad_nightlord.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '모험가 도적',
-                        style: TextStyle(
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showAdThief = !showAdThief;
+                      });
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
                           color: Colors.black,
+                          width: 2,
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage('images/ad_nightlord.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '모험가 도적',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 30),
-              ],
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showAdPirate = !showAdPirate;
+                      });
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage('images/ad_captin.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '모험가 해적',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                ],
+              ),
             ),
           ),
           Expanded(
@@ -164,99 +201,16 @@ class _JobTabState extends State<JobTab> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  if (showAdWarrior) _adWarrior(),
-                  if (showAdWizard) _adWizard(),
-                  if (showAdBowman) _adBowman(),
-                  if (showAdThief) _adThief(),
+                  if (showAdWarrior) AdJobWidgets.adWarrior(),
+                  if (showAdwizard) AdJobWidgets.adWizard(),
+                  if (showAdBowman) AdJobWidgets.adBowman(),
+                  if (showAdThief) AdJobWidgets.adThief(),
+                  if (showAdPirate) AdJobWidgets.adPirate(),
                 ],
               ),
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _adWarrior() {
-    return Container(
-      width: 400,
-      height: 600,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '첫 번째 파란색 박스',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _adWizard() {
-    return Container(
-      width: 400,
-      height: 600,
-      decoration: BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '두 번째 파란색 박스',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _adBowman() {
-    return Container(
-      width: 400,
-      height: 600,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '세 번째 파란색 박스',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _adThief() {
-    return Container(
-      width: 400,
-      height: 600,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '세 번째 파란색 박스',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
       ),
     );
   }

e8272c2208c77d4de57dc2b91d5ddb96e1d5cc02
leesanguk <sangwookpen96@gmail.com>
2023-06-18 04:10:39 +0900

    홈페이지 꾸미기 2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -42,101 +42,101 @@ class _HomePageState extends State<HomePage> {
   void _loadPosts() async {
     final List<Map<String, dynamic>> maps = await _database.query(
       'posts',
-      orderBy: 'id DESC', // 최신 순서로 정렬
+      orderBy: 'id DESC',
     );
     setState(() {
       posts = List.generate(
-        maps.length.clamp(0, 3), // 최신 3개의 글만 보이도록 제한
-            (index) =>
-            Post(
-              title: maps[index]['title'],
-              content: maps[index]['content'],
-            ),
-      );
+        maps.length,
+            (index) => Post(
+          title: maps[index]['title'],
+          content: maps[index]['content'],
+        ),
+      ).take(20).toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: Image.asset(
-            'images/background3.png',
-            fit: BoxFit.fill,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '커뮤니티 최신글',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 16),
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: posts.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue),
-                          borderRadius: BorderRadius.circular(8),
-                          color: Colors.white, // Set the background color to white
-                        ),
-                        padding: EdgeInsets.all(16),
-                        margin: EdgeInsets.only(bottom: 16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              posts[index].title,
-                              style: TextStyle(fontSize: 24),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              posts[index].content,
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                SizedBox(height: 16),
-                InkWell(
-                  onTap: () {
-                    launch('https://maplestory.nexon.com/Home/Main');
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.blue,
-                    ),
-                    child: Text(
-                      '메이플스토리 공식 홈페이지로 이동',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('홈페이지'),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/background5.png'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              '커뮤니티 최신글',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
+                ),
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.only(bottom: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      posts[index].title,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      posts[index].content,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          SizedBox(height: 16),
+          InkWell(
+            onTap: () {
+              launch('https://maplestory.nexon.com/Home/Main');
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.blue,
+              ),
+              child: Text(
+                '메이플스토리 공식 홈페이지로 이동',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+        ],
+      ),
     );
   }
 }
